f5e425df299f191499754e1cfcfa3821a05b1b2d
Avirath <142721592+Avirath08@users.noreply.github.com>
2023-09-11 00:48:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,27 +5,12 @@ const rightButton = document.getElementById('rightButton');
 const leftSound = new Audio('sounds_bongo0.mp3');
 const rightSound = new Audio('sounds_bongo1.mp3');
 
-let leftTapped = false;
-let rightTapped = false;
-
 leftButton.addEventListener('click', () => {
-    if (!leftTapped) {
-        bongoCat.src = 'Left.png'; // Changes to the left-tapping image
-        leftSound.play();
-        leftTapped = true;
-    } else {
-        bongoCat.src = 'Main.png'; // Reverts to the default image
-        leftTapped = false;
-    }
+    bongoCat.src = 'Left.png'; // Changes to the left-tapping image
+    leftSound.play();
 });
 
 rightButton.addEventListener('click', () => {
-    if (!rightTapped) {
-        bongoCat.src = 'Right.png'; // Changes to the Right-tapping image
-        rightSound.play();
-        rightTapped = true;
-    } else {
-        bongoCat.src = 'Main.png'; // Reverts to the default image
-        rightTapped = false;
-    }
+    bongoCat.src = 'Right.png'; // Changes to the Right-tapping image
+    rightSound.play();
 });
